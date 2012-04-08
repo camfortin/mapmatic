f1cc8de2d6ae8a734c6e3239ad9f2cb774bb6d5b
Cam Fortin <chamberlain.fortin@gmail.com>
2012-04-07 21:21:09 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mapmatic::Application.config.secret_token = '5345d983bfc79aee23de73eef4c44d20c4afd29b51396c980a7a8690a5de7bca9407745fbc1e3bf77223581d692cf9b7b4619e9406b0c5b5990d06bad4bba6d5'
