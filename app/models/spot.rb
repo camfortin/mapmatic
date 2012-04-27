c99f67ec01111964d5414f16174c84dae2b78a6a
Jeremiah Boyle <jeremiah@sooloos.com>
2012-04-26 22:10:31 -0700

    GeoLocation class to simplify handling gps coords

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,9 +1,24 @@
+require "geo_location"
+
 class Spot < ActiveRecord::Base
+
     attr_accessible :description, :image, :location, :title, :user
+
     has_attached_file :image,
         :styles => { :medium => "300x300>", :thumb => "100x100>" },
         :storage => :s3,
         :s3_credentials => S3_CREDENTIALS,
         :path => "images/:style/:id/:filename"
+
     validates :user, :presence => true
+
+    def location
+        # return a GeoLocation object instead of the raw string
+        GeoLocation.new(self[:location])
+    end
+
+    def location=(val)
+        self[:location] = val
+    end
+
 end
---
+++ lib/geo_location.rb
@@ -0,0 +1,50 @@
+class GeoLocation
+
+    def initialize(location_string)
+        @is_valid = false
+        @raw_string = location_string
+        if location_string != nil
+            parts = location_string.split(',')
+            if parts.count == 3
+                @latitude = parts[0]
+                @longitude = parts[1]
+                @altitude= parts[2]
+                @is_valid = true
+            end
+        end
+    end
+
+    def is_valid
+        @is_valid
+    end
+
+    def check_valid
+        if !is_valid
+            raise "GeoLocation: invalid format '#{@raw_string}'"
+        end
+    end
+
+    def latitude
+        check_valid
+        @latitude
+    end
+
+    def longitude
+        check_valid
+        @longitude
+    end
+
+    def altitude
+        check_valid
+        @altitude
+    end
+
+    def to_s
+        if is_valid
+            "#{@latitude},#{@longitude},#{@altitude}"
+        else
+            "<invalid>"
+        end
+    end
+
+end
